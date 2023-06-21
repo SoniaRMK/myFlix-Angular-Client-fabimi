e15d7b1c903ccfc48de737724137719af1187008
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-06-21 11:12:12 +0200

    Adde Registration and Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,16 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HttpClientModule,
-    UserRegistrationFormComponent
+    UserRegistrationFormComponent,
+    UserLoginFormComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -30,6 +32,7 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
     MatDialogModule,
     MatSnackBarModule,
     FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
