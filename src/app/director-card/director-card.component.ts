e9d402efce3d6739f2e5ed5ee82fa2a872ec9a85
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-07-05 15:28:00 +0200

    edited the functionality to fetch director data

--- src/app/director-card/director-card.component.ts
+++ src/app/director-card/director-card.component.ts
@@ -1,27 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { FetchApiDataService } from '../fetch-api-data.service';
+import { Component, OnInit, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import {MatDividerModule} from '@angular/material/divider' ;
 
 @Component({
-  selector: 'app-director-card',
-  templateUrl: './director-card.component.html',
-  styleUrls: ['./director-card.component.scss']
+  selector: "app-director-card",
+  templateUrl: "./director-card.component.html",
+  styleUrls: ["./director-card.component.scss"],
 })
 export class DirectorCardComponent {
-  directors: any[] = [];
-  movies: any[] = [];
-  constructor (public fetchApiData: FetchApiDataService){}
-  
-  
-  
-  
-  getDirector(directorName: string): void {
-    this.fetchApiData.getDirector(directorName).subscribe((resp : any) => {
-        this.directors = resp;
-        console.log(this.directors);
-      },
-      (error: any) => {
-        console.error(error);
-      }
-    );
-  }}
+  director: any = {};
+  constructor(@Inject(MAT_DIALOG_DATA) data: any) {
+    this.director = data;
+  }
+}
