265fed2b64aa89db015f709c62e675e33569a5f1
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-07-12 16:26:06 +0200

    deleted redundant code

--- src/app/director-card/director-card.component.spec.ts
+++ src/app/director-card/director-card.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {MatDividerModule} from '@angular/material/divider' 
+
 import { DirectorCardComponent } from './director-card.component';
 
 describe('DirectorCardComponent', () => {
